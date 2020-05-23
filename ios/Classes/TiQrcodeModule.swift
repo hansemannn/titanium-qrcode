fd512b7e06a2171d8e3c9f51eb17a244fd6d67e7
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-05-23 11:39:58 +0200

    fix(ios): fix recurrent scanning

--- ios/Classes/TiQrcodeModule.swift
+++ ios/Classes/TiQrcodeModule.swift
@@ -12,32 +12,6 @@ import TitaniumKit
 @objc(TiQrcodeModule)
 class TiQrcodeModule: TiModule {
 
-  private lazy var readerVC: QRCodeReaderViewController = {
-      let builder = QRCodeReaderViewControllerBuilder {
-          $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
-          
-          $0.showTorchButton        = false
-          $0.showSwitchCameraButton = false
-          $0.showCancelButton       = true
-          $0.showOverlayView        = false
-          $0.cancelButtonTitle = NSLocalizedString("cancel", comment: "Cancel")
-      }
-      
-      let vc = QRCodeReaderViewController(builder: builder)
-      vc.delegate = self
-      
-      return vc
-  }()
-  
-  private lazy var topMostViewController: UIViewController? = {
-    guard let controller = TiApp.controller(), let topPresentedController = controller.topPresentedController() else {
-      print("[WARN] No window opened. Ignoring gallery call …")
-      return nil
-    }
-
-    return topPresentedController
-  }()
-  
   func moduleGUID() -> String {
     return "bb7cc9ed-8591-467b-8608-be59b82bf201"
   }
@@ -66,14 +40,36 @@ class TiQrcodeModule: TiModule {
     guard let arguments = arguments, let callback = arguments.first as? KrollCallback else {
       return
     }
+    
+    let builder = QRCodeReaderViewControllerBuilder {
+        $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+        
+        $0.showTorchButton        = false
+        $0.showSwitchCameraButton = false
+        $0.showCancelButton       = true
+        $0.showOverlayView        = false
+        $0.cancelButtonTitle = NSLocalizedString("cancel", comment: "Cancel")
+    }
+    
+    let readerVC = QRCodeReaderViewController(builder: builder)
+    readerVC.delegate = self
 
     readerVC.completionBlock = { (result: QRCodeReaderResult?) in
       callback.call([["text": result?.value ?? "", "success": result != nil]], thisObject: self)
     }
 
     readerVC.modalPresentationStyle = .formSheet
-    topMostViewController?.present(readerVC, animated: true, completion: nil)
+    topMostViewController()?.present(readerVC, animated: true, completion: nil)
   }
+  
+  private func topMostViewController() -> UIViewController? {
+     guard let controller = TiApp.controller(), let topPresentedController = controller.topPresentedController() else {
+       print("[WARN] No window opened. Ignoring gallery call …")
+       return nil
+     }
+
+     return topPresentedController
+   }
 }
 
 // MARK: QRCodeReaderViewControllerDelegate
@@ -83,12 +79,12 @@ extension TiQrcodeModule : QRCodeReaderViewControllerDelegate {
   func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
     reader.stopScanning()
 
-    topMostViewController?.dismiss(animated: true, completion: nil)
+    reader.dismiss(animated: true, completion: nil)
   }
 
   func readerDidCancel(_ reader: QRCodeReaderViewController) {
     reader.stopScanning()
 
-    topMostViewController?.dismiss(animated: true, completion: nil)
+    reader.dismiss(animated: true, completion: nil)
   }
 }
